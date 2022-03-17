bbc2791550abf980e508c466ecd8f6d94e2f41b7
Sara-Jain <Sara_Jain@mckinsey.com>
2022-03-17 17:09:48 +0530

    feat: useNavigate testing

---
+++ src/components/List/List.test.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable max-len */
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import LISTS from '../../constants/list';
+import { LISTS_ROUTE } from '../../constants/routes';
+import List from './List';
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({ useNavigate: () => mockNavigate }));
+
+describe('List', () => {
+  it('should navigate to create list page when create list button is clicked', () => {
+    // const navigationMock = jest.spyOn(ReactRouterDom.useNavigate, 'useNavigate').mockImplementation(() => () => {});
+    render(<List listData={LISTS} />);
+    fireEvent.click(screen.getByText('Create list'));
+    expect(useNavigate()).toHaveBeenCalledWith(`${LISTS_ROUTE}/create`);
+    // expect(mockNavigate).toHaveBeenCalledWith(`${LISTS_ROUTE}/create`);
+  });
+});
